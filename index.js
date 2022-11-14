f27b5671e0171e209bae531a555a368954d62e02
Tushar7567 <swain.tushar00@gmail.com>
2022-11-14 13:06:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.json())
 
 app.use('/auth', require("./Route/auth"))
 
-app.listen((3000), () => {
-    console.log("Listening on port 3000")
+app.listen(process.env.PORT||8080, () => {
+    console.log("Listening on port 3000, 8080")
 })
