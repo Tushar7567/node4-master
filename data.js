2e859df0b833fd4a160facddd09e03cec26e842c
Tushar7567 <swain.tushar00@gmail.com>
2022-11-15 21:22:25 +0530

    all

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const users = [
     {
-        email: "harblaith@harb.com",
-        password: "asdasdas"
+        email: "swain756@gh.com",
+        password: "bjbbcmmm"
     }
 ]
 
--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.json())
 
 app.use('/auth', require("./Route/auth"))
 
-app.listen(process.env.PORT||8080, () => {
-    console.log("Listening on port 3000, 8080")
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
 })
